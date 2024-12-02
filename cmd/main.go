cb2d2fbc16746460b3c66004fb506422b0b44293
Takashi Asaba <nelnal@gmail.com>
2024-12-02 21:29:23 +0900

    fix: add main file
    SSIA

---
+++ cmd/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+
+	"connectrpc.com/connect"
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
+
+	greetv1 "github.com/nelnal/connect-go-test/gen/greet/v1"
+	"github.com/nelnal/connect-go-test/gen/greet/v1/greetv1connect"
+)
+
+type greetServer struct{}
+
+func (s *greetServer) Greet(
+	ctx context.Context,
+	req *connect.Request[greetv1.GreetRequest],
+) (*connect.Response[greetv1.GreetResponse], error) {
+	log.Println("Request headers: ", req.Header())
+	res := connect.NewResponse(&greetv1.GreetResponse{
+		Greeting: fmt.Sprintf("Hello, %s!", req.Msg.Name),
+	})
+	res.Header().Set("Greet-Version", "v1")
+	return res, nil
+}
+
+func main() {
+	greeter := &greetServer{}
+	mux := http.NewServeMux()
+	path, handler := greetv1connect.NewGreetServiceHandler(greeter)
+	mux.Handle(path, handler)
+	http.ListenAndServe(
+		"localhost:8080",
+		// Use h2c so we can serve HTTP/2 without TLS.
+		h2c.NewHandler(mux, &http2.Server{}),
+	)
+}
